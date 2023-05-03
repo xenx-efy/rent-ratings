ac4b03d21355781b8770d70b78f58d24ca23fc47
Alexandr Lubovskii <xenx.efy@gmai.com>
2023-05-03 21:56:26 +0300

    Create building table and seeder

--- database/seeders/BuildingSeeder.php
+++ database/seeders/BuildingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BuildingSeeder extends Seeder
 {
@@ -12,6 +13,12 @@ class BuildingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('buildings')->insert([
+           'address' => fake()->address(),
+        ]);
+
+        DB::table('buildings')->insert([
+            'address' => fake()->address(),
+        ]);
     }
 }
