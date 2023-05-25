78830be303a8d195aca8bcc88e5457ca9d31dcad
Alexandr Lubovskii <xenx.efy@gmai.com>
2023-05-25 16:27:34 +0300

    Add Roboto font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=roboto:400,500,700" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
