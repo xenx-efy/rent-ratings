e84a9b55341a7a8d2a619138e2045b1ce1dcbf3c
Alexandr Lubovskii <xenx.efy@gmai.com>
2023-08-12 16:53:55 +0300

    Fix required placeholder in RTextInput.vue

--- resources/js/components/RTextInput.vue
+++ resources/js/components/RTextInput.vue
@@ -3,14 +3,14 @@
     ref="input"
     class="w-full rounded-lg border border-silver-chalice px-4 py-2.5 placeholder:text-silver-chalice focus:outline-soft-blue"
     :placeholder="placeholder"
-  >
+  />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { defineEmits, defineExpose, defineProps, ref, withDefaults } from 'vue';
 
 interface Props {
-  placeholder: string;
+  placeholder?: string;
 }
 
 withDefaults(defineProps<Props>(), {
