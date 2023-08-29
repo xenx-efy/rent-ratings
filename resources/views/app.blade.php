eea3a59f05d267f82fab6f6a78c88212156f8a6d
Alexandr Lubovskii <40572611+xenx-efy@users.noreply.github.com>
2023-08-29 13:56:14 +0300

    Add noindex tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name') }}</title>
