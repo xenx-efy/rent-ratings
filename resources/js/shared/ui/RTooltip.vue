a7604f29c49ef96f6c14e1aed2f9cdc14cc24017
Alexandr Lubovskii <xenx.efy@gmai.com>
2023-10-01 13:10:56 +0300

    Add mouseover and mouseleave events for info icon in review form

--- resources/js/shared/ui/RTooltip.vue
+++ resources/js/shared/ui/RTooltip.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="group relative flex">
-    <question-mark-icon />
+    <question-mark-icon
+      @mouseover="hidden = false"
+      @mouseleave="hidden = true"
+    />
     <span
-      class="absolute -top-2 left-1/2 hidden -translate-x-1/2 -translate-y-full whitespace-nowrap rounded-lg bg-[#252D4F] px-2 py-1 text-center text-sm text-white after:absolute after:left-1/2 after:top-[100%] after:-mt-px after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-[#252D4F] after:content-[''] group-hover:flex"
+      class="absolute -top-2 left-1/2 -translate-x-1/2 -translate-y-full whitespace-nowrap rounded-lg bg-[#252D4F] px-2 py-1 text-center text-sm text-white after:absolute after:left-1/2 after:top-[100%] after:-mt-px after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-[#252D4F] after:content-[''] group-hover:flex"
+      :style="{ display: hidden ? 'none' : 'block' }"
     >
       {{ tooltip }}
     </span>
@@ -11,6 +15,9 @@
 
 <script setup lang="ts">
 import QuestionMarkIcon from '@/shared/icon/QuestionMarkIcon.vue';
+import { ref } from 'vue';
+
+const hidden = ref(true);
 
 interface Props {
   tooltip: string;
