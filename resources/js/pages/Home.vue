eef52d54ed09a7d26bc9957bbe925f4a13306c72
Alexandr Lubovskii <xenx.efy@gmai.com>
2024-03-12 17:03:42 +0300

    Update SEO text for home page

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,74 +1,74 @@
 <template>
   <inertia-head>
-    <title head-key="title">
-      Сервис отзывов на арендное жильё в Гродно - Rent Ratings
+    <title head-key='title'>
+      Отзывы на квартиры в Гродно - Rent Ratings
     </title>
     <meta
-      head-key="description"
-      name="description"
-      content="Просматривайте отзывы на квартиры или другое арендное жильё. Делитесь своим опытом проживания в нём. Rent Ratings помогает не ошибиться при выборе жилья."
+      head-key='description'
+      name='description'
+      content='Просматривайте отзывы на арендное жильё. Делитесь своим опытом проживания в нём. Rent Ratings помогает не ошибиться при выборе арендного жилья.'
     >
 
     <!--Чтобы гугл индексировал именно эту страницу, так как на ней есть канонический тег-->
     <link
       v-if="host === 'rent-ratings.by'"
-      rel="canonical"
-      href="https://rent-ratings.by"
+      rel='canonical'
+      href='https://rent-ratings.by'
     >
   </inertia-head>
 
   <r-header
-    search-placeholder="Введите адрес квартиры"
-    redirect-path="house"
+    search-placeholder='Введите адрес квартиры'
+    redirect-path='house'
   >
     <template #title>
       Посмотри отзывы на квартиру
     </template>
   </r-header>
 
-  <section class="relative -mt-7 flex flex-col">
-    <div class="absolute left-[-20px] top-[23%] scale-x-[-1]">
+  <section class='relative -mt-7 flex flex-col'>
+    <div class='absolute left-[-20px] top-[23%] scale-x-[-1]'>
       <search-icon
-        color="fill-blue-400"
-        width="182"
-        height="182"
+        color='fill-blue-400'
+        width='182'
+        height='182'
       />
     </div>
 
     <r-info-card
-      title="Найди квартиру по адресу"
-      :img="MapImage"
-      description="Введитe адрес дома, найдите интересующую квартиру и посмотрите отзывы на неё. Если отзывов нет, вы можете написать его 😉"
+      title='Найди квартиру по адресу'
+      :img='MapImage'
+      description='Введитe адрес дома, найдите интересующую квартиру и посмотрите отзывы на неё. Если отзывов нет, вы можете написать его 😉'
     />
   </section>
 
   <r-info-card
-    title="Напиши анонимный отзыв о жилье"
-    :img="PhoneImage"
-    description="Найди квартиру по адресу и анонимно поделись своими впечатлениями о проживании в ней."
-    theme="blue"
-    button="Оставить отзыв"
-    @click="openAddressSearch"
+    title='Напиши анонимный отзыв о жилье'
+    :img='PhoneImage'
+    description='Найди квартиру по адресу и анонимно поделись своими впечатлениями о проживании в ней.'
+    theme='blue'
+    button='Оставить отзыв'
+    @click='openAddressSearch'
   />
 
   <section>
-    <h2 class="my-8 text-center text-2xl">
+    <h2 class='my-8 text-center text-2xl'>
       Для чего этот сайт?
     </h2>
 
     <r-info-card
-      v-for="card in aboutCardArray"
-      :key="card.title"
-      :title="card.title"
-      :img="card.img"
-      :description="card.description"
-      :theme="card.theme"
-      :title-level="3"
+      v-for='card in aboutCardArray'
+      :key='card.title'
+      :title='card.title'
+      :img='card.img'
+      :description='card.description'
+      :theme='card.theme'
+      :title-level='3'
     />
   </section>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import SearchIcon from '@/shared/icon/SearchIcon.vue';
 import RHeader from '@/shared/components/RHeader.vue';
 import CatImage from '../../images/home/cat.webp';
